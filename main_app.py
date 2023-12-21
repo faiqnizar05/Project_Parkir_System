c469781ee87e8572600a67f6aa79f4e8cdbfbcb6
Fajar Muhamad <146068460+iawasome@users.noreply.github.com>
2023-12-21 21:59:20 +0700

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -1,50 +1,44 @@
-# from tkinter import * 
-# window =Tk()
-# window.mainloop()
-
 import tkinter as tk
 from datetime import datetime
 
-# Fungsi untuk mengambil waktu masuk
 def get_entry_time():
     now = datetime.now()
     entry_time = now.strftime("%H:%M:%S")
     return entry_time
 
-# Fungsi untuk membuat tiket parkir
 def generate_ticket():
     plate_number = plate_entry.get()
     entry_time = get_entry_time()
+    jenis_kendaraan = jenis_entry.get()
 
-# plat
-    ticket_info = f"Nomor Plat: {plate_number}\nWaktu Masuk: {entry_time}"
+    # Display plate number and entry time
+    ticket_info = f"Nomor Plat: {plate_number}\nJenis Kendaraan: {jenis_kendaraan}\nWaktu Masuk: {entry_time}"
     ticket_display.config(state=tk.NORMAL)
     ticket_display.delete("1.0", tk.END)
     ticket_display.insert(tk.END, ticket_info)
     ticket_display.config(state=tk.DISABLED)
 
-
-
-
-# Membuat jendela utama
 root = tk.Tk()
 root.title("Aplikasi Tiket Parkir")
 
-# Label dan input nomor plat
 plate_label = tk.Label(root, text="Masukkan Nomor Plat:")
 plate_label.pack()
 
 plate_entry = tk.Entry(root)
 plate_entry.pack()
 
-# Tombol untuk membuat tiket
+jenis_label = tk.Label(root, text="Masukkan Jenis Kendaraan:")
+jenis_label.pack()
+
+jenis_entry = tk.Entry(root)  # Fixing variable name
+jenis_entry.pack()
+
 generate_button = tk.Button(root, text="Buat Tiket", command=generate_ticket)
 generate_button.pack()
 
-# Menampilkan tiket parkir
 ticket_display = tk.Text(root, height=6, width=30)
 ticket_display.config(state=tk.DISABLED)
 ticket_display.pack()
 
-# Menjalankan aplikasi
+root.geometry("400x300")
 root.mainloop()
